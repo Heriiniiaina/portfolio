59d4e31c47c4f9ddb0c624a7aacb03247b215682
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-09-23 21:22:38 +0200

    maj deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import Navbar from './components/Navbar'
 import About from './components/About'
 import Skills from './components/Skills'
